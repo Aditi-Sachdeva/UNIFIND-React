16cd0ad534f22d4e53d07034a938eb8088a9a02e
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2025-10-13 21:44:47 +0530

    Initial Commit : Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App(){
+    return(
+    <div className="min-h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-600 text-center py-10">
+        UNIFIND
+      </h1>
+    </div>
+    );
+}
+
+export default App
