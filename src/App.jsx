55730341a1cf83dd287c3c7aa6c73bdbb1c1f911
Yashvi <aggarwalyashvi6@gmail.com>
2025-10-19 13:55:21 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import Home from "./pages/public/Home"
 import ReportItem from "./pages/public/ReportItem";
-
+import Login from "./pages/public/Login";
+import SignUp from "./pages/public/SignUp";
 
 function App(){
   return(
     <div>
-      <Home/>
+      <SignUp/>
     </div>
   );
 }
