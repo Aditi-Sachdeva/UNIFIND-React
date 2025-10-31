b77c4835e07119c9be6fc0eee023d52ee6bdc3e6
Yashvi <aggarwalyashvi6@gmail.com>
2025-10-31 22:36:55 +0530

    Updated

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,10 +1,1 @@
-<<<<<<< HEAD
-function ThemeContext(){
-    
-}
 
-export default ThemeContext;
-
-
-=======
->>>>>>> fa2b051a90e01195d364bf1e34f14c4e3dcfe423
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,3 @@ export default {
 };
 
 
-
-
-
-<<<<<<< HEAD
-=======
-
->>>>>>> fa2b051a90e01195d364bf1e34f14c4e3dcfe423
