b93eb03f2ffa56671610c91cc3454d35106bbe45
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2025-11-01 11:57:08 +0530

    Updated Report Item Form

--- src/pages/public/ReportItem.jsx
+++ src/pages/public/ReportItem.jsx
@@ -3,7 +3,7 @@ import ReportItemForm from '../../components/forms/ReportItemForm';
 
 export default function ReportItem({ user }) {
   return (
-    <div className="bg-gray-100 dark:from-gray-900 dark:to-gray-800 transition-colors duration-500 min-h-screen flex flex-col md:min-h-[calc(100vh-64px)] ">
+    <div className="bg-gray-100 dark:bg-gray-900 dark:to-gray-800 transition-colors duration-500 min-h-screen flex flex-col md:min-h-[calc(100vh-64px)] ">
       {/* Form container takes remaining height after navbar */}
       <div className="flex-1 flex justify-center items-center px-4 overflow-hidden">
         <ReportItemForm user={user} />
